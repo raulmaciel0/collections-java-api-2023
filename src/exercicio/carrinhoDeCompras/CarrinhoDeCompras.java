9453655ef4d98c8cc32c8411124b52a6d5364552
raulmaciel0 <raul.maciel015@gmail.com>
2023-10-10 20:20:12 -0300

    Resolvendo Desafio proposto

---
+++ src/exercicio/carrinhoDeCompras/CarrinhoDeCompras.java
@@ -0,0 +1,77 @@
+package exercicio.carrinhoDeCompras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        Item item = new Item(nome, preco, quantidade);
+        this.itemList.add(item);
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensASeremRemovidos = new ArrayList<>();
+
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                if (item.getNome().equalsIgnoreCase(nome)) {
+                    itensASeremRemovidos.add(item);
+                }
+            }
+            itemList.removeAll(itensASeremRemovidos);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        if (!itemList.isEmpty()) {
+            for (Item item : itemList) {
+                double valorDoItem = item.getPreco() * item.getQuantidade();
+                valorTotal += valorDoItem;
+            }
+            return valorTotal;
+        } else {
+            throw new RuntimeException("A lista está vazia");
+        }
+    }
+
+    public void exibirItens() {
+        if (!itemList.isEmpty()) {
+            System.out.println(this.itemList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+        // Adicionando itens ao carrinho
+        carrinhoDeCompras.adicionarItem("Lápis", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Caneta", 2d, 3);
+        carrinhoDeCompras.adicionarItem("Caderno", 35d, 1);
+        carrinhoDeCompras.adicionarItem("Borracha", 2d, 2);
+
+        // Exibindo os itens no carrinho
+        carrinhoDeCompras.exibirItens();
+
+        // Removendo um item do carrinho
+        carrinhoDeCompras.removerItem("Lápis");
+
+        // Exibindo os itens atualizados no carrinho
+        carrinhoDeCompras.exibirItens();
+
+        // Calculando e exibindo o valor total da compra
+        System.out.println("O valor total da compra é = " + carrinhoDeCompras.calcularValorTotal());
+    }
+}
